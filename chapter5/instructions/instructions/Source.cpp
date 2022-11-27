5bf51601247b39bf89c51514df03069ec8d8f397
annaantipovaa <antipova2004anna@mail.ru>
2022-11-27 19:09:00 +0300

    added programs from ch5

---
+++ chapter5/instructions/instructions/Source.cpp
@@ -0,0 +1,17 @@
+//Instructions 
+//demonstrates writing of new functions
+#include <iostream>
+using namespace std;
+//function prototype (declaration)
+void instructions();
+int main()
+{
+	instructions();
+	return 0;
+}
+//definition of function
+void instructions()
+{
+	cout << "Welcome to the most fun you've ever had with text!\n\n";
+	cout << "Here's how to play the game...\n";
+}
---
+++ chapter5/taking_damage/taking_damage/Source.cpp
@@ -0,0 +1,21 @@
+//Taking Damage
+//demonstrates function inlining
+#include <iostream>
+int radiation(int health);
+using namespace std;
+int main()
+{
+	int health = 80;
+	cout << "Your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is: " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is: " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is: " << health << "\n\n";
+	return 0;
+}
+inline int radiation(int health)
+{
+	return (health / 2);
+}
