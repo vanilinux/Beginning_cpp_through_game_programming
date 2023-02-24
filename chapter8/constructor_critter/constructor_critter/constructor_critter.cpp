2b5c8c2480cdaa8517a13ff3ed5b783e388d8f73
annaantipovaa <antipova2004anna@mail.ru>
2023-02-24 20:01:29 +0300

    added programs from ch 8

---
+++ chapter8/constructor_critter/constructor_critter/constructor_critter.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Critter
+{
+public:
+	int m_Hunger;
+	Critter(int Hunger = 0);
+	void Greet();
+};
+Critter::Critter(int hunger) 
+{
+	cout << "A new critter has been born!" << "\n";
+	m_Hunger = hunger;
+}
+void Critter::Greet()
+{
+	cout << "Hi, I'm a critter. My hunger level is " << m_Hunger << "\n";
+}
+
+int main()
+{
+	Critter crit(7);
+	crit.Greet();
+	return 0;
+}
+
+
---
+++ chapter8/private_critter/private_critter/private_critter.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Critter
+{
+    public:
+        Critter(int hunger = 0);
+        int GetHunger() const;
+        void SetHunger(int hunger);
+    private:
+        int m_Hunger;
+
+};
+Critter::Critter(int hunger): 
+m_Hunger(hunger)
+{
+    cout << "A new critter has been born!" << "\n";
+}
+int Critter::GetHunger() const
+{
+    return m_Hunger;
+}
+void Critter::SetHunger(int hunger)
+{
+    if (hunger < 0)
+    {
+        cout << "you can't set a critter's hunger to a negative number\n\n";
+    }
+    else
+    {
+        m_Hunger = hunger;
+    }
+}
+int main()
+{
+    Critter crit(5);
+    cout << crit.GetHunger() << "\n\n";
+    crit.SetHunger(-1);
+    crit.SetHunger(9);
+    cout << crit.GetHunger() << "\n\n";
+    return 0;
+}
+
+
